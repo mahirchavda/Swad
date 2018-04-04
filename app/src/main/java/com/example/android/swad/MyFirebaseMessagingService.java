8f410e6dbcd5c5a2c79a2a736038bb9362c1c78a
Mahir Chavda <mahir.chavda13@gmail.com>
2018-04-04 23:37:02 +0530

    Notification and Icon Done

--- app/src/main/java/com/example/android/swad/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/android/swad/MyFirebaseMessagingService.java
@@ -16,53 +16,63 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     private static final String TAG = "okokok";
 
-
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
 
-        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        //Log.d(TAG, "From: " + remoteMessage.getFrom());
 
-        String notificationTitle = null, notificationBody = null;
-        String dataTitle = null, dataMessage = null;
+        String notificationTitle = null;
+        String notificationBody = null;
+        String notificationId = null;
+        int quantity,remaining;
+        String mute = null;
 
-        if (remoteMessage.getData().size() > 0) {
-            Log.d(TAG, "Message data payload: " + remoteMessage.getData().get("message"));
-            dataTitle = remoteMessage.getData().get("title");
-            dataMessage = remoteMessage.getData().get("message");
+
+        if(remoteMessage.getData() != null){
+            Log.d(TAG, "Message data payload: " + remoteMessage.getData().get("name"));
+            notificationId = remoteMessage.getData().get("orderid");
+            notificationTitle = remoteMessage.getData().get("name");
+            mute = remoteMessage.getData().get("mute");
+            remaining = Integer.parseInt(remoteMessage.getData().get("remaining"));
+            quantity = Integer.parseInt(remoteMessage.getData().get("quantity"));
+            if (quantity - remaining - 1 == 0){
+                notificationBody = "Preparing Started";
+            } else if(mute.compareToIgnoreCase("false") == 0) {
+                notificationBody = "Order is Completed";
+            } else {
+                notificationBody = "" + (quantity-remaining - 1) + " of " + quantity + " completed";
+            }
         }
-
-        if (remoteMessage.getNotification() != null) {
-            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
-            notificationTitle = remoteMessage.getNotification().getTitle();
-            notificationBody = remoteMessage.getNotification().getBody();
-        }
-
-        sendNotification(notificationTitle, notificationBody, dataTitle, dataMessage);
+        sendNotification(notificationId,notificationTitle, notificationBody, mute);
     }
 
-    private void sendNotification(String notificationTitle, String notificationBody, String dataTitle, String dataMessage) {
+    private void sendNotification(String notificationId,String notificationTitle, String notificationBody, String mute) {
         Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra("title", dataTitle);
-        intent.putExtra("message", dataMessage);
+        intent.putExtra("title", "title");
+        intent.putExtra("message", "message");
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
                 PendingIntent.FLAG_ONE_SHOT);
 
+        long[] v = {500,1000};
+
         Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
-                .setSmallIcon(R.mipmap.ic_launcher)
+                .setSmallIcon(R.drawable.food)
                 .setContentTitle(notificationTitle)
                 .setContentText(notificationBody)
                 .setAutoCancel(true)
-                .setSound(defaultSoundUri)
                 .setContentIntent(pendingIntent);
+
+        if(mute.compareToIgnoreCase("false") == 0){
+            notificationBuilder.setSound(defaultSoundUri);
+            notificationBuilder.setVibrate(v);
+        }
 
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+        notificationManager.notify(1 , notificationBuilder.build());
 
     }
-
-
 }
